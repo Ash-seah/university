0b702efe17d74f70c4f5e109a5c407bc6466af33
parsa <parsarahimi@gmail.com>
2024-10-03 11:30:30 +0330

    remove changes

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ from database import SessionLocal
 from models import User, Classes, Riazi, Tajrobi, Ensani, Teachers
 from typing import List
 from auth import get_password_hash  # If you implement authentication features
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
+
 
 app = FastAPI()
 
-# rate limiter
-limiter = Limiter(key_func=get_remote_address)
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 REQUESTS_PER_MINUTE = 20
 ACCESS_TOKEN_EXPIRE_MINUTES = 5
@@ -65,7 +60,7 @@ class TeacherCreate(BaseModel):
 
 # Create a user (this endpoint can be extended with authentication later)
 @app.post("/users/")
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
+
 def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
     hashed_password = get_password_hash(user_data.password)  # Password hashing
     hashed_password = get_password_hash(user_data.password)
@@ -78,7 +73,6 @@ def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
 
 # Create a class
 @app.post("/classes/")
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
 def create_class(class_data: ClassCreate, db: Session = Depends(get_db)):
     new_class = Classes(name=class_data.name, description=class_data.description)
     db.add(new_class)
@@ -89,7 +83,6 @@ def create_class(class_data: ClassCreate, db: Session = Depends(get_db)):
 
 # Create Riazi associated with a class
 @app.post("/riazi/")
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
 def create_riazi(riazi_data: RiaziCreate, db: Session = Depends(get_db)):
     db_class = db.query(Classes).filter(Classes.id == riazi_data.class_id).first()
     if db_class is None:
@@ -104,7 +97,6 @@ def create_riazi(riazi_data: RiaziCreate, db: Session = Depends(get_db)):
 
 # Create Tajrobi associated with a class
 @app.post("/tajrobi/")
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
 def create_tajrobi(tajrobi_data: TajrobiCreate, db: Session = Depends(get_db)):
     db_class = db.query(Classes).filter(Classes.id == tajrobi_data.class_id).first()
     if db_class is None:
@@ -119,7 +111,7 @@ def create_tajrobi(tajrobi_data: TajrobiCreate, db: Session = Depends(get_db)):
 
 # Create Ensani associated with a class
 @app.post("/ensani/")
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
+
 def create_ensani(ensani_data: EnsaniCreate, db: Session = Depends(get_db)):
     db_class = db.query(Classes).filter(Classes.id == ensani_data.class_id).first()
     if db_class is None:
@@ -148,28 +140,26 @@ def create_teacher(teacher_data: TeacherCreate, db: Session = Depends(get_db)):
 
 # Retrieve all classes
 @app.get("/classes/", response_model=List[ClassCreate])
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
+
 def get_classes(db: Session = Depends(get_db)):
     return db.query(Classes).all()
 
 
 # Retrieve all Riazi entries
 @app.get("/riazi/", response_model=List[RiaziCreate])
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
 def get_riazi(db: Session = Depends(get_db)):
     return db.query(Riazi).all()
 
 
 # Retrieve all Tajrobi entries
 @app.get("/tajrobi/", response_model=List[TajrobiCreate])
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
+
 def get_tajrobi(db: Session = Depends(get_db)):
     return db.query(Tajrobi).all()
 
 
 # Retrieve all Ensani entries
 @app.get("/ensani/", response_model=List[EnsaniCreate])
-@limiter.limit(f"{REQUESTS_PER_MINUTE}/minute")
 def get_ensani(db: Session = Depends(get_db)):
     return db.query(Ensani).all()
 
